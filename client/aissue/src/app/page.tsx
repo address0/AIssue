aa2334572ebd68d414fdc22ab5c0e8d46b32b0c2
address0 <soyong6624@gmail.com>
2024-10-28 21:51:08 +0900

    [FE] feat: 기본 랜딩 페이지 제작  #S11P31A403-246

--- client/aissue/src/app/page.tsx
+++ client/aissue/src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>안녕</div>
+  return <div className="w-screen h-screen bg-blue-300 flex items-center justify-center">
+    <div className="animate-pulse">
+      <h2 className="text-blue-700 text-xl">주니어(아이) 개발자 전용 업무 자동화 서비스</h2>
+      <h1 className="text-white font-bold text-4xl">AIssue</h1>
+    </div>
+  </div>
 }
