a62035bbdd15d153373c3b65dc2180acb2abe3e5
최희현 <lemoncloud22@naver.com>
2024-10-29 11:10:13 +0900

    feat:login UI

--- client/aissue/src/app/page.tsx
+++ client/aissue/src/app/page.tsx
@@ -1,8 +1,20 @@
 'use client';
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Lottie from "react-lottie-player";
 import lottieJson from '@public/lottie/Animation - 1730162499899.json';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/login");
+    }, 2000); // 1초 후에 이동
+
+    return () => clearTimeout(timer); // 컴포넌트가 언마운트될 때 타이머 정리
+  }, [router]);
+
   return (
     <div className="w-screen h-screen bg-blue-300 flex items-center justify-center overflow-hidden">
       <div className="flex items-center gap-8">
