acd33fbdf31993ed20ed50268401048aabd1d6df
전태호 <jht121091@gmail.com>
2024-11-04 16:38:10 +0900

    feat: 로그인, 회원가입 시 사용자의 이름도 응답메세지에 추가

--- server/src/main/java/ssafy/aissue/api/auth/response/AuthResponse.java
+++ server/src/main/java/ssafy/aissue/api/auth/response/AuthResponse.java
@@ -14,20 +14,23 @@ public record AuthResponse(
         @JsonInclude(JsonInclude.Include.NON_NULL)
         Long memberId,
 
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        String memberName,
+
         Boolean isRegistered,
 
         @JsonInclude(JsonInclude.Include.NON_NULL)
         String email
 ) {
-    public static AuthResponse of(String accessToken, String refreshToken, Long memberId) {
+    public static AuthResponse of(String accessToken, String refreshToken, Long memberId, String memberName) {
         if (accessToken == null || refreshToken == null) {
             throw new AbnormalLoginProgressException();
         }
-        return new AuthResponse(accessToken, refreshToken,memberId, true, null);
+        return new AuthResponse(accessToken, refreshToken, memberId, memberName, true, null);
     }
 
     public static AuthResponse notRegistered(UserInfo userinfo) {
-        return new AuthResponse(null, null, null,false, userinfo.email());
+        return new AuthResponse(null, null, null,null,false, userinfo.email());
     }
 
 }
