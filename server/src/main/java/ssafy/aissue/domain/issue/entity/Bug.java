1c73783d6dd25ab933a4b70cd0b7000902184a9f
tjdgus2308 <tjdgus2308@gmail.com>
2024-11-05 09:29:44 +0900

    feat: Issue  엔°티 구현

---
+++ server/src/main/java/ssafy/aissue/domain/issue/entity/Bug.java
@@ -0,0 +1,17 @@
+package ssafy.aissue.domain.issue.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bug extends BaseIssue {
+
+}
+
+
