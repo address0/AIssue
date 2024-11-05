ce9f7bede6c2a1bed889f6265bf4daa719c1c789
최희현 <lemoncloud22@naver.com>
2024-11-05 14:45:01 +0900

    fix:build error

--- client/aissue/src/app/(Calender)/week/page.tsx
+++ client/aissue/src/app/(Calender)/week/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Sidebar from '@/components/(Navbar)/Sidebar/Sidebar'
 import Link from 'next/link'
 import ChatModalGreen from '@/components/(Modal)/ChatModalGreen/page'
-import styles from '@/app/(Calender)/week/week.module.css'
+
 
 interface Task {
   title: string
