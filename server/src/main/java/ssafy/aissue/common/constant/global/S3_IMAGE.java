d7712294e65c7872d36ddf2b8e569691da490f2e
전태호 <jht121091@gmail.com>
2024-11-12 14:09:22 +0900

    feat: project entity 수정

--- server/src/main/java/ssafy/aissue/common/constant/global/S3_IMAGE.java
+++ server/src/main/java/ssafy/aissue/common/constant/global/S3_IMAGE.java
@@ -1,5 +1,5 @@
 package ssafy.aissue.common.constant.global;
 
 public final class S3_IMAGE {
-    public static final String DEFAULT_URL = "https://ddada-image.s3.ap-northeast-2.amazonaws.com/profileImg/default.jpg";
+    public static final String DEFAULT_URL = "https://aissue-image.s3.ap-northeast-2.amazonaws.com/project/default.png";
 }
