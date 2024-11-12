ec6684f57a9998639dea99070be40369f73b6fa4
tjdgus2308 <tjdgus2308@gmail.com>
2024-11-12 15:02:29 +0900

    feat: 이슈 일정 수정 기능 추가

--- server/src/main/java/ssafy/aissue/api/issue/request/IssueScheduleRequest.java
+++ server/src/main/java/ssafy/aissue/api/issue/request/IssueScheduleRequest.java
@@ -2,14 +2,18 @@ package ssafy.aissue.api.issue.request;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-
+@Getter
 public class IssueScheduleRequest {
 
     @JsonProperty("issue_id")
     private Long issueId;
+
+    @JsonProperty("issue_key")
+    private String issueKey;
 
     @JsonProperty("start_at")
     private LocalDateTime startAt;
